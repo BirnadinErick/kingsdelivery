23c08d7fbd9c1d2bff035938f784ad25ea50575b
Birnadin Erick <45619033+BirnadinErick@users.noreply.github.com>
2024-09-28 14:53:52 +0100

    add cuisine and category id to products

---
+++ database/migrations/2024_09_28_135117_add_missing_ids_to_products.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('products', function (Blueprint $table) {
+            $table->unsignedBigInteger('category_id');
+            $table->unsignedBigInteger('cuisine_id');
+
+            $table->foreign('category_id')->references('category_id')->on('categorys')->onDelete('cascade');
+            $table->foreign('cuisine_id')->references('cuisine_id')->on('cuisines')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('products', function (Blueprint $table) {
+            $table->dropForeign(['cuisine_id']);
+            $table->dropForeign(['category_id']);
+        });
+    }
+};
